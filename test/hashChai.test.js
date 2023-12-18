0707ec2d6a6bfabb3944f5566b0bd0e2490ddd02
MauricioAlonso <program.alonso@gmail.com>
2023-12-18 18:02:21 -0300

    agregado el testing de hash con chai

--- test/hashChai.test.js
+++ test/hashChai.test.js
@@ -1,8 +1,43 @@
-const mongoose = require("mongoose");
-const config = require("../src/config/config.js");
-// const Assert = require("assert");
+// const mongoose = require("mongoose");
+//const config = require("../src/config/config.js");
 const chai = require("chai");
-const UsersMongoDao = require("../src/dao/usersMongoDao.js");
+//const UsersMongoDao = require("../src/dao/usersMongoDao.js");
 const { describe, it } = require("mocha");
+const { generaHash, validaHash } = require("../src/util.js");
 
-mongoose.connect(config.MONGO_URL, { dbName: config.DB_NAME });
+//mongoose.connect(config.MONGO_URL, { dbName: config.DB_NAME });
+
+
+const expect=chai.expect
+
+describe("Pruebas funciones hash", async ()=>{
+    //this.timeout(5000)
+
+    it("Si ejecuto generaHash enviando un password como argumento, genera un hash con algoritmo Bcrypt", async ()=>{
+        let password="codercoder"
+        let resultado=await generaHash(password)
+
+        expect(resultado).not.to.be.equal(password)
+        expect(resultado.length).to.be.greaterThan(10)
+        expect(resultado.substring(0,4)).to.be.equal("$2b$")
+    })
+
+    it("La funcion validaHash recibe usuario y password, verifica coincidencia", async ()=>{
+        let password ="123"
+        let usuario = {
+            password: generaHash(password)
+        }
+        let resultado = await validaHash(usuario, password)
+
+        expect(resultado).is.true
+        expect(resultado).to.be.eq(true)
+
+       resultado = await validaHash(usuario, "567");
+
+        expect(resultado).is.false;
+        expect(resultado).to.be.eq(false);
+        
+    })
+})
+
+//npx mocha ./test/hashChai.test.js
